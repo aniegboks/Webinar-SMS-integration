be942d9791d3e81692a5c4309bfe37b6ff43dfc3
aniegboks <aniegbokavictor2024@gmail.com>
2026-04-28 15:16:46 +0100

    change port address

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 import { postRoutes } from "./routes";
 
-const port_address = process.env.PORT_ADDRESS || 3000;
+const port_address = process.env.PORT || 3000;
 const app = new Elysia().use(postRoutes).listen(port_address);
 
 console.log(
